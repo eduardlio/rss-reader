6cbbb2d8f4ca70fda2d55a5f28f79cf6cc5f55bc
eduardlio <liao.edward@gmail.com>
2018-04-13 23:00:32 +1000

    something is working yayayay

--- feed-read/index.js
+++ feed-read/index.js
@@ -1,1 +1,11 @@
 var reader = require("feed-reader");
+var parse = reader.parse;
+var url = 'https://news.ycombinator.com/rss';
+parse(url).then((feed) => {
+   console.log(feed);
+}).catch((err) => {
+   console.error("error");
+}).finally(() => {
+   console.log("everything done");
+});
+
