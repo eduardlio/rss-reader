04677a69f8e211e00b21700e2b779b9812dd10b6
eduardlio <liao.edward@gmail.com>
2018-04-13 23:18:29 +1000

    outputs to html now

--- aj.js
+++ aj.js
@@ -7,20 +7,29 @@ $(document).ready(function(){
    $("button").on("click", (thing) => {
       var id = thing.target.id;
       var url = urls[id];
-      getFeed(url);
+      getFeed(id, url);
    });
 });
-
-function getFeed(url){
+function getFeed(id, url){
    var feed = PROXY + url;
    fetch(feed).then( (res) => {
       res.text().then( (xmlText) => {
          var domParser = new DOMParser();
-         console.log(xmlText);
          let doc = domParser.parseFromString(xmlText, "text/xml");
+         $(".output").html("");
          doc.querySelectorAll("item").forEach( (item) => {
-            console.log("item: " + item.querySelector("title").textContent); 
+            var title = getQuerySelectorText(item, "title");
+            var link = getQuerySelectorText(item, "link");
+            $(".output").append("<a href='"+link+"' class='news-title'>"+title+"</h1>");
+            $(".output").append("<br>");
+            if(id=="hp"){
+               var desc = getQuerySelectorText(item, "description");
+               $(".output").append("<p class='news-desc'>"+desc+"</p>");
+            }
          });
       })
    }).catch( () => console.error("error occurred"));
+}
+function getQuerySelectorText(item, selector){
+   return item.querySelector(selector).textContent;
 }
